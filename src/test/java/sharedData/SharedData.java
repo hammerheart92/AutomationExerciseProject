8708a942da43ed19b7c9b24fffea7903464907fe
hammerheart92 <hammerheart92@gmail.com>
2025-03-29 11:59:51 +0200

    Retest ci/cd

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -12,15 +12,20 @@ public class SharedData {
 
     public void setUpDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // Run without GUI
+        options.addArguments("--headless"); // Run without GUI
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
         options.addArguments("--window-size=1920x1080"); // Set the window size to full HD
+        options.addArguments("--disable-gpu"); // Disable GPU hardware acceleration (useful for CI environments)
+        options.addArguments("--remote-allow-origins=*"); // Allow all origins for remote connections
+
 
         driver = new ChromeDriver(options);
         driver.get("https://www.automationexercise.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        System.out.println("Browser setup completed and opened the URL: " + driver.getCurrentUrl());
     }
 
     public WebDriver getDriver(){
