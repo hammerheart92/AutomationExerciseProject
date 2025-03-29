3fe2d769a8c8cc5b536ab5253c39b36ae6706bd6
hammerheart92 <hammerheart92@gmail.com>
2025-03-29 13:34:20 +0200

    Implemented browser service

---
+++ src/test/java/browserService/BrowserService.java
@@ -0,0 +1,7 @@
+package browserService;
+
+public interface BrowserService {
+
+    void openBrowser ();
+    Object prepareOptions ();
+}
--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -1,26 +1,29 @@
 package sharedData;
 
+import browserService.ChromeService;
+import browserService.EdgeService;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.time.Duration;
 
 public class SharedData {
 
     public WebDriver driver;
 
     public void setUpDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // Run in headless mode for CI
-        options.addArguments("--no-sandbox"); // Required for GitHub Actions
-        options.addArguments("--disable-dev-shm-usage"); // Prevents memory issues
-        options.addArguments("--window-size=1920,1080"); // Set a default window size
 
-        driver = new ChromeDriver(options);
-        driver.get("https://www.automationexercise.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String browser = "edge";
+
+        switch (browser){
+            case "edge":
+                EdgeService edgeService = new EdgeService();
+                edgeService.openBrowser();
+                driver = edgeService.getDriver();
+                break;
+            case "chrome":
+                ChromeService chromeService = new ChromeService();
+                chromeService.openBrowser();
+                driver = chromeService.getDriver();
+                break;
+        }
     }
 
     public WebDriver getDriver(){
